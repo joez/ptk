51f214abeeaaa5fec04b0de98660411ca084e30b
Joe Zheng <joe.zheng.cn@gmail.com>
2017-04-29 17:43:39 +0800

    add doc for Ptk::Gex::Manifest and a minor fix

--- lib/Ptk/Gex/Manifest.pm
+++ lib/Ptk/Gex/Manifest.pm
@@ -94,6 +94,9 @@ sub add_project {
   my $name = shift or die;
   my $attr = shift or die;
 
+  # fix the attr if no "name"
+  $attr->{name} = $name unless $attr->{name};
+
   # there is no "path" defined for mirror project
   my $path = $attr->{path} || $name;
   my $data = $self->_project_data;
@@ -196,6 +199,9 @@ sub add_remote {
   my $name = shift or die;
   my $attr = shift or die;
 
+  # fix the attr if no "name"
+  $attr->{name} = $name unless $attr->{name};
+
   my $data = $self->_remote_data;
 
   push @{$self->_remotes}, $name unless $data->{$name};
@@ -274,3 +280,131 @@ sub _parse {
 
 1;
 __END__
+
+=encoding utf8
+
+=head1 NAME
+
+Ptk::Gex::Manifest - Manifest file for repo tool
+
+=head1 SYNOPSIS
+
+  use Ptk::Gex::Manifest;
+
+  # load manifest and print out the "path" of each project
+  my $m = Ptk::Gex::Manifest->new('.repo/manifests/default.xml');
+  for my $n ($m->list_project_names) {
+    my $r = $m->get_resolved_project($n);
+    say $r->{path};
+  }
+
+=head1 DESCRIPTION
+
+L<Ptk::Manifest> is a class for loading, parsing and saving manifest file used
+by L<repo|https://gerrit.googlesource.com/git-repo> tool.
+
+=head1 ATTRIBUTES
+
+L<Ptk::Manifest> implements the following attributes.
+
+=head2 path
+
+  my path = $m->path;
+  my $m   = $m->path('path');
+
+The file path of the manifest, defaults to '.repo/manifests/default.xml'.
+
+=head1 METHODS
+
+L<Ptk::Manifest> implements the following methods.
+
+=head2 load
+
+  $m->load;
+  $m->load('path');
+
+Load the manifest with the given file path or default one.
+
+=head2 save
+
+  $m->save;
+  $m->save('path');
+
+Save the manifest into the file, or save-as the given file path.
+
+The target file will be a flattened manifest.
+
+=head2 add_project
+
+  $m = $m->add_project('foo', { name => 'foo', path => 'bar' });
+
+Add a project with its C<name> and a hash reference for attributes.
+
+The B<name> attribute is mandatory for a project, will be fixed automatically
+if it is missing.
+
+=head2 del_project
+
+  $m = $m->del_project('foo');
+
+Delete a project with its C<name>.
+
+=head2 get_project
+
+  my $p = $m->get_project('foo');
+
+Get a project with its C<name> or C<path>.
+
+=head2 get_resolved_project
+
+  my $p = $m->get_resolved_project('foo');
+
+Get a resolved project with its C<name> or C<path>.
+
+A resolved project means that all of its default values are resolved, such as
+remote URL, etc.
+
+=head2 get_project_name_by_path
+
+  my $n = $m->get_project_name_by_path('foo');
+
+Get the C<name> of the project given its C<apth>.
+
+=head2 list_project_names
+
+  my $ns = $m->list_project_names;
+
+Get an arrary reference including all the project's names.
+
+=head2 list_project_paths
+
+  my $ps = $m->list_project_paths;
+
+Get an arrary reference including all the project's paths.
+
+=head2 add_remote
+
+  $m = $m->add_remote('foo', { name => 'foo', fetch => 'bar' });
+
+Add a remote with its C<name> and a hash reference for attributes.
+
+The B<name> attribute is mandatory for a remote, will be fixed automatically
+if it is missing.
+
+=head2 get_remote
+
+  my $r = $m->get_remote('foo');
+
+Get a remote with its C<name>.
+
+=head2 list_remote_names
+
+  my $rns = $m->list_remote_names;
+
+Get an arrary reference including all the remote's name.
+
+=head1 SEE ALSO
+
+L<Ptk>, L<Ptk::Gex>, L<https://github.com/joez/ptk>.
+
+=cut
