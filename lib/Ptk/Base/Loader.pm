64190829ae1fdc75af059567e54c6bc1d1e4ee93
Joe Zheng <joe.zheng.cn@gmail.com>
2017-04-30 09:32:58 +0800

    refactoring for Ptk::Base::Loader

--- lib/Ptk/Base/Loader.pm
+++ lib/Ptk/Base/Loader.pm
@@ -1,15 +1,52 @@
 # author: joe.zheng
 
 package Ptk::Base::Loader;
-use base 'Mojo::Loader';
 use Ptk::Base -base;
 
-Mojo::Loader->import(qw/data_section file_is_binary load_class find_modules/);
+use Mojo::Util qw/monkey_patch/;
 
-sub data      { shift; data_section(@_) }
-sub is_binary { shift; file_is_binary(@_) }
-sub load      { shift; load_class(@_) }
-sub search { shift; [find_modules(@_)] }
+# we don't want to import any functions
+require Mojo::Loader;
+
+my @subs
+  = qw/data_section file_is_binary load_class find_modules find_packages/;
+for my $m (@subs) {
+  no strict 'refs';
+  my $s = \&{"Mojo::Loader::$m"};
+  monkey_patch(__PACKAGE__, $m, sub { shift; $s->(@_) });
+}
 
 1;
 __END__
+
+=encoding utf8
+
+=head1 NAME
+
+Ptk::Base::Loader - Load all kinds of things
+
+=head1 SYNOPSIS
+
+  use Ptk::Base::Loader;
+
+  my $ldr = Ptk::Base::Loader->new;
+  # Find modules in a namespace
+  for my $module ($ldr->find_modules('Some::Namespace')) {
+
+    # Load them safely
+    my $e = $ldr->load_class($module);
+    warn qq{Loading "$module" failed: $e} and next if ref $e;
+
+    # And extract files from the DATA section
+    say $ldr->data_section($module, 'some_file.txt');
+  }
+
+=head1 DESCRIPTION
+
+L<Ptk::Base::Loader> is a class wrapper of L<Mojo::Loader>
+
+=head1 SEE ALSO
+
+L<Ptk>, L<Mojo::Loader>, L<https://github.com/joez/ptk>.
+
+=cut
