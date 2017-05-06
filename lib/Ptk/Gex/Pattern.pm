4999aff16036511359d9151e8945f0346c9a6aed
Joe Zheng <joe.zheng.cn@gmail.com>
2017-05-07 00:13:08 +0800

    add doc for Ptk::Pattern and Ptk::Gex::Pattern

--- lib/Ptk/Gex/Pattern.pm
+++ lib/Ptk/Gex/Pattern.pm
@@ -3,12 +3,6 @@
 package Ptk::Gex::Pattern;
 use Ptk::Base 'Ptk::Pattern';
 
-# there are multiple query items in the query
-# if one of the items match, the result is true
-# if all the items fail to match, the result is false
-#
-# for each item, there are multiple keys to match
-# if one of the key is not match, the whole item fails
 sub match {
   my $self = shift;
   my $info = shift;
@@ -53,3 +47,71 @@ ITEM: for my $item (@$p) {
 
 1;
 __END__
+
+=encoding utf8
+
+=head1 NAME
+
+Ptk::Gex::Pattern - Pattern to match
+
+=head1 SYNOPSIS
+
+  use Ptk::Pattern;
+
+  my $obj = Ptk::Pattern->new('remote:aosp name:platform,!path:vendor');
+  say 'matched' if $obj->match($object);
+
+=head1 DESCRIPTION
+
+L<Ptk::Gex::Pattern> is a pattern can be used to match a object, with a query
+specifying which fields must match what patterns
+
+There will be multiple query items in the query, separated with B<SPACE>, if
+one of them match, the result is true, if B<all> of them fail to match, the
+result is false
+
+For each item, there will be multiple fields to match, separated with "B<,>",
+the field name and pattern are separated with "B<:>", if one of the field does
+not match, the whole item fails
+
+Here is an example: C<remote:aosp name:platform,!path:vendor>
+
+Which means:
+
+=over 4
+
+=item *
+
+If the object's field C<remote> matches "aosp", the object matches
+
+=item *
+
+B<OR> if the object's field C<name> matches "platform" B<AND> field C<path>
+does not match "vendor", then the object matches
+
+=item *
+
+Otherwise the object is failed to match
+
+=back
+
+=head1 ATTRIBUTES
+
+L<Ptk::Gex::Pattern> inherits all attributes from L<Ptk::Pattern>.
+
+=head1 METHODS
+
+L<Ptk::Gex::Pattern> inherits all methods from L<Ptk::Pattern> and overrides
+the following ones.
+
+=head2 match
+
+  my $matched = $obj->match({name => 'platform/build', path => 'build'});
+
+Check whether the object is matched by the pattern.
+
+=head1 SEE ALSO
+
+L<Ptk>, L<Ptk::Pattern>, L<https://github.com/joez/ptk>.
+
+=cut
--- lib/Ptk/Pattern.pm
+++ lib/Ptk/Pattern.pm
@@ -35,3 +35,49 @@ sub match {
 
 1;
 __END__
+
+=encoding utf8
+
+=head1 NAME
+
+Ptk::Pattern - Pattern to match
+
+=head1 SYNOPSIS
+
+  use Ptk::Pattern;
+
+  my $obj = Ptk::Pattern->new($query);
+  say 'matched' if $obj->match($data);
+
+=head1 DESCRIPTION
+
+L<Ptk::Pattern> is a pattern can be used to match some data
+
+=head1 ATTRIBUTES
+
+L<Ptk::Pattern> inherits all attributes from L<Ptk::Base> and implements the
+following new ones.
+
+=head2 query
+
+  my $q = $obj->query;
+  $obj  = $obj->query('regex');
+
+The query string to be used, it will be built into regex internally
+
+=head1 METHODS
+
+L<Ptk::Pattern> inherits all methods from L<Ptk::Base> and implements the
+following new ones.
+
+=head2 match
+
+  my $matched = $obj->match('data to be check');
+
+Check whether the string is matched by the pattern.
+
+=head1 SEE ALSO
+
+L<Ptk>, L<Ptk::Base>, L<https://github.com/joez/ptk>.
+
+=cut
