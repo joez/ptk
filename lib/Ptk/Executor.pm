8c16a1fa4aace05bc84df7cd1577b2b8adfc57bc
Joe Zheng <joe.zheng.cn@gmail.com>
2017-05-14 00:26:27 +0800

    add doc and test for Ptk::Executor

--- lib/Ptk/Executor.pm
+++ lib/Ptk/Executor.pm
@@ -22,8 +22,8 @@ sub execute {
 
   my $fm = Parallel::ForkManager->new($self->workers);
 
-  # ($idx, $ttl);
-  $fm->run_on_start(sub { $self->_on_start($_[1], $ttl) });
+  # ($idx);
+  $fm->run_on_start(sub { $self->_on_start($_[1]) });
 
   # ($idx, $exit_code, $data);
   $fm->run_on_finish(sub { $self->_on_finish(@_[qw/2 1 5/]) });
@@ -41,7 +41,7 @@ sub execute {
     $self->command->($idx, $self->context, $job, $exit);
 
     # child exit if not yet
-    exit;
+    $exit->(0);
   }
 
   return $fm->wait_all_children;
@@ -50,7 +50,7 @@ sub execute {
 sub _on_start {
   my $self = shift;
 
-  my ($idx, $ttl) = @_;
+  my ($idx) = @_;
 
   return $self->on_start->($idx, $self->context);
 }
@@ -65,3 +65,146 @@ sub _on_finish {
 
 1;
 __END__
+
+=encoding utf8
+
+=head1 NAME
+
+Ptk::Executor - execute tasks in the workers
+
+=head1 SYNOPSIS
+
+  use Ptk::Executor;
+
+  my @tasks     = (1 .. 10);
+  my $context   = { ok => [], done => [] };
+  my $command   = sub {
+    my ($idx, $ctx, $task, $exit) = @_;
+    $exit->(0, [$idx]);
+  };
+  my $on_finish = sub {
+    my ($idx, $ctx, $code, $data) = @_;
+    if (!$code) {
+      push @{$ctx->{ok}}, $idx;
+      push @{$ctx->{done}}, $data->[0] if defined $data;
+    }
+  };
+
+  my $executor = Ptk::Executor->new(
+    workers   => 5,
+    context   => $context,
+    command   => $command,
+    on_finish => $on_finish,
+  )->execute(@tasks);
+
+  say 'ok: '   . join(', ', @{$context->{ok}});
+  say 'done: ' . join(', ', @{$context->{done}});
+
+=head1 DESCRIPTION
+
+L<Ptk::Executor> execute tasks in a limited number of workers and get the
+results
+
+=head1 ATTRIBUTES
+
+L<Ptk::Executor> inherits all attributes from L<Ptk::Base> and implements the
+following new ones.
+
+=head2 command
+
+  my $cmd = sub {
+    my ($idx, $ctx, $task, $exit) = @_;
+
+    say "worker ($idx) is doing $task ...";
+
+    # finish the task, return the exit code and data
+    $exit->(0, \$idx);
+  };
+
+  $executor->command($cmd);
+  $executor->command;
+
+The callback to run for the tasks, it will be executed in the workers
+(B<child processes>), with for following parameters:
+
+=over 2
+
+=item *
+
+C<$idx> - the index of the tasks
+
+=item *
+
+C<$ctx> - the L<context>
+
+=item *
+
+C<$task> - the given task
+
+=item *
+
+C<$exit> - a callback to finish the worker, with optional parameters
+B<exit code> and B<data reference> (any valid reference should be OK)
+
+=back
+
+=head2 context
+
+  $executor->context({ ttl => 10 });
+  $executor->context;
+
+The context which can be accessed when execute the C<command>
+
+Because the workers are running in the child processes, changes made on
+context when execute the C<command>, can't be accessed by parent process
+
+=head2 on_start
+
+  my $on_start = sub {
+    my ($idx, $ctx) = @_;
+
+    say "worker ($idx) is started ...";
+  };
+
+  $executor->on_start($on_start);
+  $executor->on_start;
+
+The callback called when a task is started
+
+=head2 on_finish
+
+  my $on_finish = sub {
+    my ($idx, $ctx, $code, $data) = @_;
+
+    say "worker ($idx) is finished with exit code $code";
+  };
+
+  $executor->on_finish($on_finish);
+  $executor->on_finish;
+
+The callback called when a task is finished, providing the B<exit code> and
+B<data> (a reference) return from the worker
+
+=head2 workers
+
+  $executor->workers(5);
+  $executor->workers;
+
+The maximum number of workers, the default is 1.
+
+=head1 METHODS
+
+L<Ptk::Executor> inherits all methods from L<Ptk::Base> and implements the
+following new ones.
+
+=head2 execute
+
+  $executor->execute(1 .. 10);
+
+Execute the given tasks
+
+=head1 SEE ALSO
+
+L<Ptk>, L<Ptk::Base>, L<https://github.com/joez/ptk>.
+
+=cut
---
+++ t/ptk/executor.t
@@ -0,0 +1,64 @@
+use Ptk::Base -strict;
+
+use Test::More;
+
+use FindBin;
+use lib "$FindBin::Bin/lib";
+
+use Ptk::Executor;
+use Storable qw/dclone/;
+
+my $workers  = 5;
+my @tasks    = (1 .. 10);
+my $context  = {start => [], done => [], ok => []};
+my $command  = sub { my ($idx, $ctx, $task, $exit) = @_; $exit->(0, \$idx) };
+my $on_start = sub {
+  my ($idx, $ctx) = @_;
+  push @{$ctx->{start}}, $idx;
+};
+my $on_finish = sub {
+  my ($idx, $ctx, $code, $data) = @_;
+  if (!$code) {
+    push @{$ctx->{ok}}, $idx;
+    push @{$ctx->{done}}, $$data if defined $data;
+  }
+};
+
+my $executor = Ptk::Executor->new(
+  workers   => $workers,
+  command   => $command,
+  context   => dclone($context),
+  on_start  => $on_start,
+  on_finish => $on_finish,
+);
+
+# API
+can_ok $executor, qw/workers context command on_start on_finish execute/;
+
+# Normal usage
+is $executor->workers,        $workers,   'get workers';
+is_deeply $executor->context, $context,   'get context';
+is $executor->command,        $command,   'get command';
+is $executor->on_start,       $on_start,  'get on_start';
+is $executor->on_finish,      $on_finish, 'get on_finish';
+
+$executor->execute(@tasks);
+is scalar @{$executor->context->{start}}, scalar @tasks, 'tasks started';
+is scalar @{$executor->context->{done}},  scalar @tasks, 'tasks done';
+is_deeply $executor->context->{ok}, $executor->context->{done}, 'ok and done';
+
+# Do nothing in the command
+$executor->context(dclone($context))->command(sub { })->execute(@tasks);
+is scalar @{$executor->context->{start}}, scalar @tasks,
+  'tasks started with nop';
+is scalar @{$executor->context->{ok}}, scalar @tasks, 'tasks ok with nop';
+is scalar @{$executor->context->{done}}, 0, 'tasks done with nop';
+
+# Die in the command
+$executor->context(dclone($context))->command(sub {die})->execute(@tasks);
+is scalar @{$executor->context->{start}}, scalar @tasks,
+  'tasks started with die';
+is scalar @{$executor->context->{ok}},   0, 'tasks ok with die';
+is scalar @{$executor->context->{done}}, 0, 'tasks done with die';
+
+done_testing();
